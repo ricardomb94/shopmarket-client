df88d1cb81414efb70129bb8f889d41ade755082
yohan <yohan.cauvry@blgcloud.com>
2020-05-28 12:37:13 +0200

    fix create category

--- src/Shop/AddCategory.js
+++ src/Shop/AddCategory.js
@@ -15,13 +15,13 @@ const AddCategory = () => {
 
 
 //On destruture user à partir du localstorage
-    // const {userId} = isAuth(); 
+    // const {userId} = isAuth();
     // console.log(userId)
    const token = getCookie('token');
 //    console.log('TOKEN FRONT',token)
    const userId = isAuth();
     // console.log('userId=>', userId);
-    
+
     const handleChange =  (e) => {
         e.preventDefault()
         setError('')
@@ -31,46 +31,46 @@ const AddCategory = () => {
        e.preventDefault()
        setError('')
        setSuccess(false)
-       
+
        //Reaquette API pour créer les catégories
-     CreateCategory(userId, token, {name})
-         
+     CreateCategory(userId._id, token, {name})
+
     };
 
     const newCategoryForm = () => (
-    
+
         <form  onSubmit={clickSubmit}>
             <div className="form-control">
                 <div className="jumbotron catego text-center text-uppercase text-black font-x-small">
                     <Zoom right cascade><h4>La création c'est l'intelligence qui s'amuse</h4></Zoom>
                         <Pulse><hr className="bg-danger underline"/></Pulse></div>
                             <label className="text-muted">Nom</label>
-                                <input 
-                                    type="text" 
-                                    className="form-control" 
-                                    onChange={handleChange} 
+                                <input
+                                    type="text"
+                                    className="form-control"
+                                    onChange={handleChange}
                                     value={name}
                                     autoFocus
                                     />
                                     <br/>
                                 <button type="submit" className="btn btn-outline-danger">Créer une catégorie
-                                </button>    
+                                </button>
                 </div>
         </form>
     );
-    
+
     const showSuccess = () => {
         if(success){
             return <h4 className="text-success">{name} viens d'être créer</h4>;
         }
     }
-    
+
     const showError = () => {
         if(error){
             return <h4 className="text-danger">{name} Le nom doit être unique</h4>;
         }
     }
-    
+
     return(
         <Layout title="Ajouter une nouvelle categorie" className="container">
             <div className="row">
@@ -81,8 +81,8 @@ const AddCategory = () => {
                 </div>
             </div>
         </Layout>
-        
-        
+
+
     );
 };
 
--- src/Shop/CreateCategory.js
+++ src/Shop/CreateCategory.js
@@ -1,29 +1,28 @@
 import {isAuth, getCookie} from '../auth/helpers';
 import axios from "axios";
 
-  const checkAuth = isAuth();
-  const token = getCookie('token');
-  console.log(token)
-  
- const CreateCategory = (_id, token,category) => {
- console.log(category)
-        return (
-                
-                axios({
-                    method: 'POST',
-                    url: `${process.env.REACT_APP_API}/category/create`,
-                    headers: {
-                      Accept: "application/json",
-                      Authorization:`Bearer ${token}`
-                    },data:{'category': 'namaaaa'}
-                })
-                .then(response => {
-                    console.log('ADD CATEGORY RESP.', response);
-                })
-                .catch(error => {
-                    console.log('ADD CATEGORY ERROR', error) 
-                })
-        )}
-        
+const checkAuth = isAuth();
+const token = getCookie('token');
+console.log(token)
 
-export default  CreateCategory      
+const CreateCategory = (_id, token, category) => {
+    return (
+        axios({
+            method: 'POST',
+            url: `${process.env.REACT_APP_API}/category/create/${_id}`,
+            headers: {
+                Accept: "application/json",
+                Authorization: `Bearer ${token}`
+            }, data: {category}
+        })
+            .then(response => {
+                console.log('ADD CATEGORY RESP.', response);
+            })
+            .catch(error => {
+                console.log('ADD CATEGORY ERROR', error)
+            })
+    )
+}
+
+
+export default CreateCategory
